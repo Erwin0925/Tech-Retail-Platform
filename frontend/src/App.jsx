1cbd246601e297ee3299a79aad4180c3f415133b
Erwin <erwin925.07@gmail.com>
2024-11-15 22:25:06 +0800

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import { Button } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import { useColorModeValue } from '@chakra-ui/react'
 
 function App() {
   return (
-    <>
-      <Button>Hello</Button>
-    </>
-  )
-}0
+    <Box minH = {"100vh"} bg={useColorModeValue("gray.100","gray.900")}>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/create" element={<CreatePage />}/>
+      </Routes>
+    </Box>
+  );
+}
 
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>
 );
