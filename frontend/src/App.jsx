fce7a211f07c9fb682b6d4df9339499950e87a46
Erwin <erwin925.07@gmail.com>
2024-11-15 14:28:25 +0800

    frontend environment setup - p1

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@chakra-ui/react"
+
+function App() {
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  )
+}0
+
+export default App
---
+++ frontend/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+import App from './App';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </StrictMode>
+);
